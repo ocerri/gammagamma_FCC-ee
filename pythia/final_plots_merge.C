5972f029ed4ad57b2debe800571a8b6c5af529d3
apodo <alessandro.podo@outlook.com>
2015-07-27 20:09:29 +0200

    Added data from OPAL for dsdpt; comparison with simulation; Added notes in a box

--- pythia/final_plots_merge.C
+++ pythia/final_plots_merge.C
@@ -23,6 +23,7 @@
 #include "TLegend.h"
 #include "TColor.h"
 #include "TPaveText.h"
+#include "TPaveStats.h"
 
 
 
@@ -37,40 +38,42 @@ TH1F* h_dsdpt30 = 0;
 ///Start merging procedure------------------------------------------
 void final_plots_merge(){
 
- // TFile* file10_dsdpt = new TFile("_root/MSTP14-10_final_out.root");
- // h_dsdpt10 = (TH1F*)file10_dsdpt->Get("hdsigmadpT");
- // h_dsdpt10->SetMarkerColor(2);
- // h_dsdpt10->SetLineColor(2);
- // h_dsdpt10->SetTitle("PYTHIA simulation of d#sigma/dp_{t} for e^{+}e^{-} #rightarrow h^{#pm} at 160 GeV");
+ TFile* file10_dsdpt = new TFile("_root/MSTP14-10_final_out.root");
+ h_dsdpt10 = (TH1F*)file10_dsdpt->Get("hdsigmadpT");
+ h_dsdpt10->SetMarkerColor(2);
+ h_dsdpt10->SetLineColor(2);
+ h_dsdpt10->SetAxisRange(0.01,50000,"Y");
+ h_dsdpt10->SetTitle("PYTHIA simulation of d#sigma/dp_{t} for e^{+}e^{-} #rightarrow h^{#pm} at 160 GeV");
  
  
- // TFile* file30_dsdpt = new TFile("_root/MSTP14-30_final_out.root");
- // h_dsdpt30 = (TH1F*)file30_dsdpt->Get("hdsigmadpT");
- // h_dsdpt30->SetLineColor(4);
- // //h_dsdpt30->SetLineStyle(4);
- // h_dsdpt30->SetTitle("PYTHIA simulation of d#sigma/dp_{t} for {e}^{+} {e}^{-} -> {h}^{#pm} at 160 GeV");
+ TFile* file30_dsdpt = new TFile("_root/MSTP14-30_final_out.root");
+ h_dsdpt30 = (TH1F*)file30_dsdpt->Get("hdsigmadpT");
+ h_dsdpt30->SetLineColor(4);
+ h_dsdpt30->SetMarkerColor(4);
+ h_dsdpt10->SetAxisRange(0.01,50000,"Y");
+ h_dsdpt30->SetTitle("PYTHIA simulation of d#sigma/dp_{t} for {e}^{+} {e}^{-} -> {h}^{#pm} at 160 GeV");
 
- TFile* file10_dsdeta = new TFile("_root/MSTP14-10_final.root");
- h_dsdeta10 = (TH1F*)file10_dsdeta->Get("hdsigmadeta");
- h_dsdeta10->SetMarkerColor(2);
- h_dsdeta10->SetLineColor(2);
- h_dsdeta10->SetAxisRange(8.,24.,"Y");
- h_dsdeta10->SetTitle("PYTHIA simulation of d#sigma/deta for e^{+}e^{-} #rightarrow h^{#pm} at 160 GeV");
+ // TFile* file10_dsdeta = new TFile("_root/MSTP14-10_final.root");
+ // h_dsdeta10 = (TH1F*)file10_dsdeta->Get("hdsigmadeta");
+ // h_dsdeta10->SetMarkerColor(2);
+ // h_dsdeta10->SetLineColor(2);
+ // h_dsdeta10->SetAxisRange(8.,24.,"Y");
+ // h_dsdeta10->SetTitle("PYTHIA simulation of d#sigma/deta for e^{+}e^{-} #rightarrow h^{#pm} at 160 GeV");
  
  
- TFile* file30_dsdeta = new TFile("_root/MSTP14-30_final.root");
- h_dsdeta30 = (TH1F*)file30_dsdeta->Get("hdsigmadeta");
- h_dsdeta30->SetMarkerColor(4);
- //h_dsdeta30->SetLineColor(4);
- h_dsdeta30->SetAxisRange(8.,24.,"Y");
- h_dsdeta30->SetTitle("PYTHIA simulation of d#sigma/deta for {e}^{+} {e}^{-} -> {h}^{#pm} at 160 GeV");
+ // TFile* file30_dsdeta = new TFile("_root/MSTP14-30_final.root");
+ // h_dsdeta30 = (TH1F*)file30_dsdeta->Get("hdsigmadeta");
+ // h_dsdeta30->SetMarkerColor(4);
+ // //h_dsdeta30->SetLineColor(4);
+ // h_dsdeta30->SetAxisRange(8.,24.,"Y");
+ // h_dsdeta30->SetTitle("PYTHIA simulation of d#sigma/deta for {e}^{+} {e}^{-} -> {h}^{#pm} at 160 GeV");
 
  //Plotting experimental data from txt-------------------------------------------
 
  std::ifstream myfile;
- myfile.open("_txt/dsdeta_opal.txt");
+ myfile.open("_txt/dsdpt_opal.txt");
 
- const Int_t n_data = 5; //number of data (rows) in .txt
+ const Int_t n_data = 21; //number of data (rows) in .txt
 
  Double_t x[n_data];
  Double_t y[n_data];
@@ -79,16 +82,10 @@ void final_plots_merge(){
 
  int j=0;
 
- //Errors on x are null
- for(j=0; j< n_data; j++){
-   ex[j]=0;
- }
-
- j=0;
-
  //Read data from file .txt
  while(true) {
    myfile >> x[j];
+   myfile >> ex[j];
    myfile >> y[j];
    myfile >> ey[j];
    if( myfile.eof() ) break;
@@ -109,42 +106,38 @@ void final_plots_merge(){
 
  //Draw on Canvas---------------------------------------------------
    
- // TCanvas* c_dsdpt = new TCanvas("c_dsdpt","dsigmadipt",700,700);
- // //gStyle->SetOptStat(0);
- // c_dsdpt->cd();
+ TCanvas* c_dsdpt = new TCanvas("c_dsdpt","dsigmadipt",700,700);
+ //gStyle->SetOptStat(0);
+ c_dsdpt->cd();
 
- // h_dsdpt10->Draw();
- // h_dsdpt30->Draw("same");
- // c_dsdpt->SetLogy();
- // c_dsdpt->SetGridx();
- // c_dsdpt->SetGridy();
-
- // TLegend* leg = new TLegend(0.1,0.7,0.48,0.9);
- // leg->SetHeader("Legend");
- // leg->AddEntry(h_dsdpt10,"Histogram MSTP(14)=10","lep");
- // leg->AddEntry(h_dsdpt30,"Histogram MSTP(14)=30","lep");
- // //leg->AddEntry("gr","Graph with error bars","lep");
- // leg->Draw();
-
-
- TCanvas* c_dsdeta = new TCanvas("c_dsdeta","dsigmadeta",700,700);
- gStyle->SetOptStat(0);
- c_dsdeta->cd();
-
- h_dsdeta10->Draw();
- h_dsdeta30->Draw("same");
+ h_dsdpt10->Draw();
+ h_dsdpt30->Draw("same");
  gr->Draw("P");
- c_dsdeta->SetGridx();
- c_dsdeta->SetGridy();
+ c_dsdpt->SetLogy();
+ c_dsdpt->SetGridx();
+ c_dsdpt->SetGridy();
+
+ // TCanvas* c_dsdeta = new TCanvas("c_dsdeta","dsigmadeta",700,700);
+ // //gStyle->SetOptStat(0);
+ // c_dsdeta->cd();
+
+ // h_dsdeta10->Draw();
+ // h_dsdeta30->Draw("same");
+ // gr->Draw("P");
+ // c_dsdeta->SetGridx();
+ // c_dsdeta->SetGridy();
 
  //Building Legend-----------------------------------------------------
 
  TLegend* leg = new TLegend(0.1,0.7,0.48,0.9);
  //leg->SetHeader("Legend");
- leg->AddEntry(h_dsdeta10,"Histogram MSTP(14)=10","lep");
- leg->AddEntry(h_dsdeta30,"Histogram MSTP(14)=30","lep");
+ leg->AddEntry(h_dsdpt10,"Histogram MSTP(14)=10","lep");
+ leg->AddEntry(h_dsdpt30,"Histogram MSTP(14)=30","lep");
  leg->AddEntry("gr","Experimental data from OPAL","lep");
  leg->Draw();
+
+ //Adding comments in the statistics Box
+
 
  // TPaveText *pt = new TPaveText(.05,.1,.95,.8);
  // pt->AddText("A TPaveText can contain severals line of text.");
@@ -153,5 +146,36 @@ void final_plots_merge(){
  // pt->AddText("Even complex TLatex formulas can be added:");
  // pt->AddText("F(t) = #sum_{i=-#infty}^{#infty}A(i)cos#[]{#frac{i}{t+i}}");
  // pt->Draw();
+
+ TPaveStats *ps = (TPaveStats*)c_dsdpt->GetPrimitive("stats");
+ ps->SetName("mystats");
+ TList *list = ps->GetListOfLines();
+
+ // Remove the statistics lines
+ TText *tconst = ps->GetLineWith("RMS");
+ list->Remove(tconst);
+ tconst = ps->GetLineWith("hdsigmadpT");
+ list->Remove(tconst);
+ tconst = ps->GetLineWith("Entries");
+ list->Remove(tconst); 
+ tconst = ps->GetLineWith("Mean");
+ list->Remove(tconst); 
+
+ // Add a new line in the stat box.
+ TLatex *myt = new TLatex(0,0,"#cbar#eta#cbar< 1.5");
+ myt ->SetTextFont(42);
+ myt ->SetTextSize(0.01);
+ list->Add(myt);
+ myt = new TLatex(0,0,"10<W<125 GeV");
+ myt ->SetTextFont(42);
+ myt ->SetTextSize(0.01);
+ list->Add(myt);
+
+ // the following line is needed to avoid that the automatic redrawing of stats
+ h_dsdpt10->SetStats(0);
+ h_dsdpt30->SetStats(0);
+
+ c_dsdpt->Modified();
+
 
 }
